b8e3e7be4d3ef1dfacf5d41b8f5e701e9c69466b
Jonh Snap <jonhsnap123@gmail.com>
2022-04-07 00:18:13 +0700

    resolve missing

---
+++ MarvicSolution/MarvicSolution.DATA/MigrationManager.cs
@@ -0,0 +1,34 @@
+﻿using MarvicSolution.DATA.EF;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarvicSolution.DATA
+{
+    public static class MigrationManager
+    {
+        public static IHost MigrateDatabase(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                using (var appContext = scope.ServiceProvider.GetRequiredService<MarvicDbContext>())
+                {
+                    try
+                    {
+                        appContext.Database.Migrate();
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+            return host;
+        }
+    }
+}
