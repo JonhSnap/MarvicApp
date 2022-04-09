b064bf95296aee1d84329231f47eb76c02e2cdba
Tungnguyen038 <19130260@st.hcmuaf.edu.vn>
2022-04-09 16:20:47 +0700

    Tung add auth

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,30 @@
-import React, { lazy, Suspense } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Main from './components/layouts/Main';
+import React, { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Home from "./components/home/Home";
+// import Main from './components/layouts/Main';
 
-const YourWord = lazy(() => import('./pages/YourWordPage'));
-const ProjectsPage = lazy(() => import('./pages/ProjectsPage'));
+// const YourWord = lazy(() => import('./pages/YourWordPage'));
+// const ProjectsPage = lazy(() => import('./pages/ProjectsPage'));
 
 function App() {
   return (
-    <Suspense fallback={<>Component fallback</>}>
+    <>
       <Routes>
-        <Route element={<Main></Main>}>
-          <Route path={'/'} element={<YourWord></YourWord>}></Route>
-          <Route path={'/projects'} element={<ProjectsPage></ProjectsPage>}></Route>
-        </Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
       </Routes>
-    </Suspense>
+    </>
+    // <Suspense fallback={<>Component fallback</>}>
+    //   <Routes>
+    //     <Route element={<Main></Main>}>
+    //       <Route path={'/'} element={<YourWord></YourWord>}></Route>
+    //       <Route path={'/projects'} element={<ProjectsPage></ProjectsPage>}></Route>
+    //     </Route>
+    //   </Routes>
+    // </Suspense>
   );
 }
 
