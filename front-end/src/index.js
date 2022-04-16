d5de1f01daae17f08faab1894f9c3d3cb1c2cb2f
vothang104 <19130203@st.hcmuaf.edu.vn>
2022-04-16 21:28:47 +0700

    delete redux

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,16 +4,12 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from 'react-router-dom'
-import store from './redux/store'
-import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <React.StrictMode>
-  <Provider store={store}>
     <BrowserRouter>
           <App />
     </BrowserRouter>
-  </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
