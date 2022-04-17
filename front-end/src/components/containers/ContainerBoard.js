3e3e09c1b236f2df8dc9ce0761696b642d697a3e
vothang104 <19130203@st.hcmuaf.edu.vn>
2022-04-17 17:44:14 +0700

    add member

--- front-end/src/components/containers/ContainerBoard.js
+++ front-end/src/components/containers/ContainerBoard.js
@@ -1,24 +1,55 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import useModal from '../../hooks/useModal';
+import AddMemberPopup from '../popup/AddMemberPopup';
 import './ContainerBoard.scss'
 
-function ContainerBoard() {
+function ContainerBoard({ project }) {
+    const [show, setShow, handleClose] = useModal();
+    const [focus, setFocus] = useState(false);
+    const inputRef= useRef();
+
+    const handleFocus = () => {
+        setFocus(true);
+    }
+    const handleBlur = () => {
+        setFocus(false);
+    }
+
+    useEffect(() => {
+        const inputEl = inputRef.current;
+        inputEl.addEventListener('focus', handleFocus);
+        inputEl.addEventListener('blur', handleBlur);
+
+        return () => {
+            inputEl.removeEventListener('focus', handleFocus);
+            inputEl.removeEventListener('blur', handleBlur);
+        }
+    }, [])
+
+    const handleClickAdd = () => {
+        setShow(true);
+    }
+
   return (
     <div className='container'>
         <div className="top">
+            { show && <AddMemberPopup project={project} setShow={setShow} onClose={handleClose}></AddMemberPopup>}
             <div className="navigate">
                 <span>Projects</span>
                 <span>/</span>
-                <span>Name's project</span>
+                <span>{project.name}</span>
             </div>
             <div className="wrap-key">
-                <h3 className="key">PA board</h3>
+                <h3 className="key">{project.key} board</h3>
                 <svg xmlns="http://www.w3.org/2000/svg" className="icon" fill="none" viewBox="0 0 24 24" stroke="#ccc" strokeWidth={2}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
                 </svg>
             </div>
             <div className="actions">
-                <div className="wrap-input">
-                    <input type="text" />
+                <div className={`wrap-input ${focus ? 'expand' : ''}`}>
+                    <input
+                    placeholder={focus ? 'Search this board' : ''}
+                    ref={inputRef} type="text" />
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon" viewBox="0 0 20 20" fill="#ccc">
                     <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
                     </svg>
@@ -27,13 +58,16 @@ function ContainerBoard() {
                     <div className="avatar">
                         <img src="https://images.unsplash.com/photo-1644982647708-0b2cc3d910b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
                     </div>
-                    <div className="add-member">
+                    <div onClick={handleClickAdd} className="add-member">
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon" fill="none" viewBox="0 0 24 24" stroke="#999" strokeWidth={2}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
                     </svg>
                     </div>
                 </div>
             </div>
+        </div>
+        <div className="bottom">
+
         </div>
     </div>
   )
