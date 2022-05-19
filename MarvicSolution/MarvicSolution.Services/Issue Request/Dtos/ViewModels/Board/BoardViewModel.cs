e40a421f814354736566b12ad2ef1caf90e404b1
Jonh Snap <jonhsnap123@gmail.com>
2022-05-19 16:06:34 +0700

    fix GetIssueForBoard, GroupIssueForBoardByAssignee add filter by type and epic for GetIssueForBoard, GroupIssueForBoardByAssignee

--- MarvicSolution/MarvicSolution.Services/Issue Request/Dtos/ViewModels/Board/BoardViewModel.cs
+++ MarvicSolution/MarvicSolution.Services/Issue Request/Dtos/ViewModels/Board/BoardViewModel.cs
@@ -9,18 +9,26 @@ namespace MarvicSolution.Services.Issue_Request.Dtos.ViewModels.Board
 {
     public class BoardViewModel
     {
-        public List<Guid?> ListStageOrder { get; set; }
-        public List<StageViewModel> ListStage { get; set; }
-        public List<int> ListType { get; set; }
-        public List<Issue_ViewModel> ListEpic { get; set; }
+        public IList<Guid> ListStageOrder { get; set; }
+        public IList<StageViewModel> ListStage { get; set; }
+        public IList<int> ListType { get; set; }
+        public IList<Issue_ViewModel> ListEpic { get; set; }
 
 
         public BoardViewModel()
         {
-            this.ListStageOrder = new List<Guid?>();
+            this.ListStageOrder = new List<Guid>();
             this.ListStage = new List<StageViewModel>();
             this.ListType = new List<int>() { 2, 3, 4 };
             this.ListEpic = new List<Issue_ViewModel>();
+        }
+
+        public BoardViewModel(IList<Guid> listStageOrder, IList<StageViewModel> listStage, IList<int> listType, IList<Issue_ViewModel> listEpic)
+        {
+            ListStageOrder = listStageOrder;
+            ListStage = listStage;
+            ListType = listType;
+            ListEpic = listEpic;
         }
     }
 }
