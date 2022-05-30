c0a7d99ff5f6bfdc4ef394af03c1f0818cdcd631
vothang104 <19130203@st.hcmuaf.edu.vn>
2022-05-30 16:45:29 +0700

    add function refresh board

--- front-end/src/components/board/Board.js
+++ front-end/src/components/board/Board.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { v4 } from 'uuid';
 import axios from 'axios';
 import sorter from '../../util/sorter';
@@ -11,10 +11,32 @@ import { BASE_URL } from '../../util/constants'
 
 
 function Board({ board, project, currentSprint }) {
+    const iconRef = useRef();
     const [, dispatchBoard] = useBoardContext();
     const { listStageOrder, listStage } = board;
     sorter(listStage, listStageOrder);
 
+    // handle refresh
+    const handleRefresh = () => {
+        iconRef.current.animate(
+            [
+                { transform: 'rotate(0)', color: '#009B77' },
+                { transform: 'rotate(360deg)', color: '#009B77' }
+            ],
+            {
+                duration: 500,
+                iterations: 3,
+                endDelay: 100,
+            }
+        )
+        setTimeout(() => {
+            fetchBoard({
+                idSprint: currentSprint.id,
+                idEpic: null,
+                type: 0
+            }, dispatchBoard);
+        }, 1600);
+    }
     // handle column drop
     const handleColumnDrop = async (dropResult) => {
         const { addedIndex, removedIndex } = dropResult;
@@ -39,6 +61,13 @@ function Board({ board, project, currentSprint }) {
     }
     return (
         <div className='board'>
+            <div onClick={handleRefresh} className="refresh">
+                <span title='Refresh' ref={iconRef} className='icon'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+                    </svg>
+                </span>
+            </div>
             <Container
                 orientation="horizontal"
                 dragHandleSelector=".header-selector"
