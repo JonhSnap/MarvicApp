3791baaeb34f386325e66d9a152a176b6e1b1e7a
Tungnguyen038 <19130260@st.hcmuaf.edu.vn>
2022-06-07 00:06:06 +0700

    add archive

---
+++ front-end/.history/src/components/archive/ArchiveSprint_20220606182439.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ArchiveIssue from "./ArchiveIssue";
+import { v4 } from "uuid";
+
+const ArchiveSprint = ({ ArchiveSprint }) => {
+  console.log("ArchiveSprint", ArchiveSprint);
+  return (
+    <div className="flex flex-col mt-4 border-2 border-blue-500 rounded-lg p-">
+      <div className="flex items-center text-base">
+        <span className="cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-5 h-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </span>
+        <h2>{ArchiveSprint.sprintName}</h2>
+      </div>
+      <div className="flex flex-col items-center">
+        {ArchiveSprint.issues.map((item) => (
+          <ArchiveIssue key={v4()}></ArchiveIssue>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ArchiveSprint;
---
+++ front-end/.history/src/components/archive/ArchiveSprint_20220606190505.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import ArchiveIssue from "./ArchiveIssue";
+import { v4 } from "uuid";
+
+const ArchiveSprint = ({ ArchiveSprint }) => {
+  const [showIssue, setShowIssue] = useState(true);
+  console.log("ArchiveSprint", ArchiveSprint);
+  return (
+    <div className="flex flex-col mt-4 rounded-lg">
+      <div className="flex items-center justify-center w-full py-2 text-2xl text-white rounded-lg cursor-pointer archive-header bg-violet-500 hover:opacity-90">
+        <h2 className="ml-4 text-2xl font-semibold">
+          {ArchiveSprint.sprintName}
+        </h2>
+        <span className="ml-3">( {ArchiveSprint.issues.length} Issues)</span>
+      </div>
+      {showIssue && (
+        <div
+          id="style-16"
+          className="flex flex-col pt-2 h-auto overflow-y-auto border-l-2 border-slate-300 shadow-lg pl-2 pb-2 max-h-[280px]"
+        >
+          {ArchiveSprint.issues.map((item) => (
+            <ArchiveIssue key={v4()} ArchiveIssue={item}></ArchiveIssue>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ArchiveSprint;
