0f8792078e15492f59021e223d1723e398d34e50
thanhnhan <thanhnhan77620@gmail.com>
2022-07-26 07:58:45 +0700

    fix file system

--- MarvicSolution/MarvicSolution.Services/System/Helpers/Jwt_Service.cs
+++ MarvicSolution/MarvicSolution.Services/System/Helpers/Jwt_Service.cs
@@ -1,10 +1,8 @@
-﻿using MarvicSolution.DATA.Entities;
-using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,22 +12,13 @@ namespace MarvicSolution.Services.System.Helpers
     {
         private string secureKey = "this is a very secure key";
 
-        public string GenerateToken(App_User user)
+        public string GenerateToken(Guid IdUser)
         {
             // Header
             var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secureKey));
             var credentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
             var header = new JwtHeader(credentials);
 
-            // claims
-            var claims = new[]
-            {
-                new Claim(ClaimTypes.Surname, user.UserName),
-                new Claim(ClaimTypes.Email, user.EmailAddress),
-                new Claim(ClaimTypes.GivenName, user.GivenName),
-                new Claim(ClaimTypes.Surname, user.Surname),
-                new Claim(ClaimTypes.Role, user.Role)
-            };
             // Payload
             var payload = new JwtPayload(IdUser.ToString(), null, null, null, DateTime.Now.AddDays(2));
 
